74bc328b203cd007ac0435c208dc66391f727806
Ikiru Yoshizaki <yoshizaki@nextscape.net>
2018-12-23 02:38:01 +0900

    fix(wsl) ubuntu ansible  install

--- scripts/wsl_ubuntu/install_ansible.sh
+++ scripts/wsl_ubuntu/install_ansible.sh
@@ -4,5 +4,7 @@ which ansible > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo Ansible not found, install it.
     sudo apt-get update
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
     sudo apt-get install ansible
 fi
