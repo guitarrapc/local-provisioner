b5229d334d70918e4ab83c051a215e45d01a659f
Ikiru Yoshizaki <yoshizaki@nextscape.net>
2018-12-23 02:41:21 +0900

    change(wsl) auto install

--- scripts/wsl_ubuntu/install_ansible.sh
+++ scripts/wsl_ubuntu/install_ansible.sh
@@ -6,5 +6,5 @@ if [ $? -ne 0 ]; then
     sudo apt-get update
     sudo apt-get install software-properties-common
     sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo apt-get install ansible
+    sudo apt-get install -y ansible
 fi
