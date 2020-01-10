ab265bd48d222326d573a004cb3635d34e589305
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-10 15:40:50 +0900

    feat: change ansible install from apt to pip3

--- prerequisites/ubuntu/install.sh
+++ prerequisites/ubuntu/install.sh
@@ -4,7 +4,6 @@ which ansible > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo Ansible not found, install it.
     sudo apt-get update
-    sudo apt-get install -y software-properties-common
-    sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo apt-get install -y ansible
+    sudo apt-get install -y python3-pip
+    pip3 install --user ansible
 fi
