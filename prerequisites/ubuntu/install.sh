ff8907414f0f16a8c69d1258f6a31ef803baa651
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-05-28 11:59:45 +0900

    feat: use snap instead of pip3

--- prerequisites/ubuntu/install.sh
+++ prerequisites/ubuntu/install.sh
@@ -3,7 +3,8 @@
 which ansible > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo Ansible not found, install it.
-    sudo apt-get update
-    sudo apt-get install -y python3-pip
-    pip3 install --user ansible
+    sudo apt update
+    sudo apt install snapd
+    sudo snap install hello-world
+    hello-world
 fi
