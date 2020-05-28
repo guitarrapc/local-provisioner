36fdd700c121c8a9d5435e69e565a0f6c7eed85c
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-05-28 13:19:58 +0900

    fix: use pip3

--- envs/ubuntu/prerequisites.sh
+++ envs/ubuntu/prerequisites.sh
@@ -6,11 +6,18 @@ if [ $? -ne 0 ]; then
     sudo apt update & sudo apt upgrade -y
     sudo apt install -yqq daemonize dbus-user-session fontconfig
 
+    # install ansible on python3
+    sudo apt-get install -y python3-pip
+    pip3 install --user ansible
+
+    # prepare snap
+    sudo apt install -y snapd
     sudo cp ./sbin/start-systemd-namespace /usr/sbin/start-systemd-namespace & sudo chmod +x /usr/sbin/start-systemd-namespace
     sudo cp ./sbin/enter-systemd-namespace /usr/sbin/enter-systemd-namespace & sudo chmod +x /usr/sbin/enter-systemd-namespace
     sudo sed -i 2a"# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/bash.bashrc
-
-    sudo apt install -y snapd
-    sudo snap install hello-world
-    hello-world
 fi
+
+echo "installing ansible & snap complete."
+echo "log out and try "
+echo "$ sudo snap install hello-world"
+echo "$ hello-world"
