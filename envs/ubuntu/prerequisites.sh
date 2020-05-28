a7ae74388f9f109aa94700ceee1014e58d21d9fa
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-05-28 12:53:19 +0900

    feat: add snap systemd support on wsl2

--- envs/ubuntu/prerequisites.sh
+++ envs/ubuntu/prerequisites.sh
@@ -3,8 +3,14 @@
 which ansible > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo Ansible not found, install it.
-    sudo apt update
-    sudo apt install snapd
+    sudo apt update & sudo apt upgrade -y
+    sudo apt install -yqq daemonize dbus-user-session fontconfig
+
+    sudo cp ./sbin/start-systemd-namespace /usr/sbin/start-systemd-namespace & sudo chmod +x /usr/sbin/start-systemd-namespace
+    sudo cp ./sbin/enter-systemd-namespace /usr/sbin/enter-systemd-namespace & sudo chmod +x /usr/sbin/enter-systemd-namespace
+    sudo sed -i 2a"# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/bash.bashrc
+
+    sudo apt install -y snapd
     sudo snap install hello-world
     hello-world
 fi
