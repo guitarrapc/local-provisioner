fb7385b0c9043cf25b50177b76b31bc8a7a773a7
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2021-02-07 21:55:53 +0900

    feat: add cask

--- envs/macos-silicon/roles/homebrew/spec/homebrew_spec.rb
+++ envs/macos-silicon/roles/homebrew/spec/homebrew_spec.rb
@@ -5,13 +5,9 @@ describe command("brew list -l") do
     its(:stdout) { should match "ansible" }
     its(:stdout) { should match "autoconf" }
     its(:stdout) { should match "awscli" }
-    its(:stdout) { should match "aws-iam-authenticator" }
-    its(:stdout) { should match "azure-cli" }
     its(:stdout) { should match "bash-completion" }
     its(:stdout) { should match "bat" }
-    its(:stdout) { should match "brew-cask-completion" }
     its(:stdout) { should match "carthage" }
-    its(:stdout) { should match "circleci" }
     its(:stdout) { should match "ctop" }
     its(:stdout) { should match "direnv" }
     its(:stdout) { should match "docker" }
@@ -22,15 +18,12 @@ describe command("brew list -l") do
     its(:stdout) { should match "go" }
     its(:stdout) { should match "hub" }
     its(:stdout) { should match "hugo" }
-    its(:stdout) { should match "kubectx" }
-    its(:stdout) { should match "kubernetes-helm" }
-    its(:stdout) { should match "mackerel-agent" }
-    its(:stdout) { should match "nodebrew" }
+    its(:stdout) { should match "jq" }
+    its(:stdout) { should match "nodeenv" }
     its(:stdout) { should match "pyenv" }
     its(:stdout) { should match "rbenv" }
     its(:stdout) { should match "ruby-build" }
     its(:stdout) { should match "stern" }
-    its(:stdout) { should match "swiftlint" }
     its(:stdout) { should match "tfenv" }
     its(:stdout) { should match "tree" }
     its(:stdout) { should match "yarn" }
---
+++ envs/macos-silicon/roles/homebrew_cask/spec/homebrewcask_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+set :backend, :exec
+
+describe command("brew cask list -1") do
+    its(:stdout) { should match "docker" }
+    its(:stdout) { should match "google-chrome" }
+    its(:stdout) { should match "google-cloud-sdk" }
+    its(:stdout) { should match "gitkraken" }
+    its(:stdout) { should match "kindle" }
+    its(:stdout) { should match "krisp" }
+    its(:stdout) { should match "ngrok" }
+    its(:stdout) { should match "powershell" }
+    its(:stdout) { should match "slack" }
+    its(:stdout) { should match "unity-hub" }
+    its(:stdout) { should match "visual-studio-code" }
+end
+
