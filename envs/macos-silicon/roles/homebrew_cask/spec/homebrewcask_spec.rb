ca06d4697c17f0fe4a92ac63a1a8f2fd3859b434
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2021-02-07 23:55:48 +0900

    chore: remove app not run on m1

--- envs/macos-silicon/roles/homebrew_cask/spec/homebrewcask_spec.rb
+++ envs/macos-silicon/roles/homebrew_cask/spec/homebrewcask_spec.rb
@@ -2,13 +2,11 @@ require 'spec_helper'
 set :backend, :exec
 
 describe command("brew cask list -1") do
-    its(:stdout) { should match "docker" }
     #its(:stdout) { should match "dotnet-sdk" }
     its(:stdout) { should match "google-chrome" }
     its(:stdout) { should match "gitkraken" }
     its(:stdout) { should match "kindle" }
     #its(:stdout) { should match "krisp" }
-    its(:stdout) { should match "ngrok" }
     #its(:stdout) { should match "powershell" }
     its(:stdout) { should match "slack" }
     its(:stdout) { should match "unity-hub" }
