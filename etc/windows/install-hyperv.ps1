13aa34a5007a40f6261063be75d069560aa9f3b5
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2021-03-01 23:43:01 +0900

    feat: remove pulumi, circleci

--- etc/windows/install-hyperv.ps1
+++
@@ -1,5 +0,0 @@
-#Requires -RunAsAdministrator
-$state = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
-if ($state.State -ne "Enabled") {
-    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
-}
---
+++ etc/windows/install-windowsfeature.ps1
@@ -0,0 +1,8 @@
+#Requires -RunAsAdministrator
+$features = @("Microsoft-Hyper-V-All", "Microsoft-Windows-Subsystem-Linux")
+foreach ($feature in $features) {
+  $state = Get-WindowsOptionalFeature -Online -FeatureName $feature
+  if ($state.State -ne "Enabled") {
+    Enable-WindowsOptionalFeature -Online -FeatureName $feature
+  }
+}
