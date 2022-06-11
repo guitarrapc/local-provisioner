2e3d76a527b1fe960670a481ac7914b4aec478a5
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-06-11 23:08:47 +0900

    fix: install ansible via ppa

--- envs/ubuntu/prerequisites.sh
+++ envs/ubuntu/prerequisites.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -ex
 
 function hack:wsl1_ubuntu20() {
-    is_wsl2=$([ $(grep -oE 'gcc version ([0-9]+)' /proc/version | awk '{print $3}') -gt 5 ] && echo "1" || echo "0")
-    is_ubuntu2004=$(lsb_release -r | grep 20.04 | wc -l)
-    if [[ "$is_wsl2" == "0" && "$is_ubuntu2004" == "1" ]]; then
+    if lshw | grep vsyscall32; then
+      is_wsl2=1
+    else
+      is_wsl2=0
+    fi
+    is_ubuntu2004=$(lsb_release -r | grep -c "20.04")
+    if [[ "${is_wsl2}" == "0" && "${is_ubuntu2004}" == "1" ]]; then
       # sleep cannot use in WSL1 Ansible hack
       echo "detected Ubuntu 20.04 on WSL1, applying sleep hack. https://stackoverflow.com/questions/62363901/ansible-msg-failed-to-create-temporary-directory-in-some-cases-fatal"
       sudo mv /bin/sleep /bin/sleep~
@@ -11,23 +15,13 @@ function hack:wsl1_ubuntu20() {
     fi
 }
 
-function install_ansible_before_ubuntu22() {
-  if ! lsb_release -r | grep -V 22.04; then
-    # install ansible on python3
-    echo "Install ansible from pip3."
-    sudo apt install -y python3-pip
-    pip3 install --user ansible
-  fi
-}
-
-function install_ansible_ubuntu22() {
-  if lsb_release -r | grep 22.04; then
-    # Install Ansible from ppa for latest release and fast update.
-    echo "Install ansible from ppa."
-    sudo apt install -y software-properties-common
-    sudo add-apt-repository -y --update ppa:ansible/ansible
-    sudo apt install -y ansible
-  fi
+function install_ansible() {
+  # Install Ansible from ppa for latest release and fast update.
+  # `pip3 install --user ansible` is too slow and could not accept.
+  echo "Install ansible from ppa."
+  sudo apt install -y software-properties-common
+  sudo add-apt-repository -y --update ppa:ansible/ansible
+  sudo apt install -y ansible
 }
 
 if ! which ansible; then
@@ -36,8 +30,7 @@ if ! which ansible; then
     sudo apt upgrade -y
 
     # install ansible
-    install_ansible_before_ubuntu22
-    install_ansible_ubuntu22
+    install_ansible
 
     # Ubuntu20.04LTS on WSL1
     hack:wsl1_ubuntu20
