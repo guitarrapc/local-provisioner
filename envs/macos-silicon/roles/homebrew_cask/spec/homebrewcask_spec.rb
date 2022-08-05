d5105cd4b60759f40f929debdad1cb578c7bdd24
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-08-05 12:34:26 +0900

    feat: use cask for app installation

--- envs/macos-silicon/roles/homebrew_cask/spec/homebrewcask_spec.rb
+++ envs/macos-silicon/roles/homebrew_cask/spec/homebrewcask_spec.rb
@@ -3,15 +3,15 @@ set :backend, :exec
 
 describe command("brew cask list -1") do
     its(:stdout) { should match "android-studio" }
-    #its(:stdout) { should match "dotnet-sdk" }
+    its(:stdout) { should match "docker" }
+    its(:stdout) { should match "dotnet-sdk" }
     its(:stdout) { should match "google-chrome" }
     its(:stdout) { should match "gitkraken" }
     its(:stdout) { should match "kindle" }
-    #its(:stdout) { should match "krisp" }
-    #its(:stdout) { should match "powershell" }
+    its(:stdout) { should match "krisp" }
+    its(:stdout) { should match "powershell" }
     its(:stdout) { should match "slack" }
     its(:stdout) { should match "unity-hub" }
     its(:stdout) { should match "visual-studio-code" }
-    #its(:stdout) { should match "zoomus" }
+    its(:stdout) { should match "zoomus" }
 end
-
