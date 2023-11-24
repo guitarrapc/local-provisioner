5466aec4ddcd33706420e8b434b926ceeb953803
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-11-24 10:49:48 +0900

    tmp: back.rb

---
+++ envs/macos/roles/terraform/spec/terraform_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+set :backend, :exec
+
+describe command("which tfenv") do
+    its(:exit_status) { should eq 0 }
+end
+
+describe command("tfenv list") do
+    its(:stdout) { should match "0.11.10" }
+end
