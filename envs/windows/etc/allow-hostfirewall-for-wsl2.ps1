605198a2637c93f8cf441779cd5aca8334ec8bad
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-06-13 15:42:59 +0900

    feat: 2 type of adapter

--- envs/windows/etc/allow-hostfirewall-for-wsl2.ps1
+++ envs/windows/etc/allow-hostfirewall-for-wsl2.ps1
@@ -12,15 +12,21 @@ foreach ($item in $(netsh interface portproxy show v4tov4 | grep "127.0.0.1")) {
 # launch wsl2
 wsl.exe -d Ubuntu-22.04 --exec cat /etc/resolv.conf
 
-# disable Windows Firewall for WSL2 Interface
-Set-NetFirewallProfile -DisabledInterfaceAliases "vEthernet (WSL)" -Name Private
+$adapters = $("vEthernet (WSL)", "vEthernet (WSL (Hyper-V firewall))")
+foreach ($adapter in $adapters) {
 
-# Allow WSL2 to access Windows Docker Daemon to share same images.
-$wslAddress = (Get-NetIPAddress -InterfaceAlias "vEthernet (WSL)" -AddressFamily IPv4).IPAddress
-iex "netsh interface portproxy add v4tov4 listenport=2375 connectaddress=127.0.0.1 connectport=2375 listenaddress=$wslAddress protocol=tcp"
+  # disable Windows Firewall for WSL2 Interface
+  Set-NetFirewallProfile -DisabledInterfaceAliases "$adapter" -Name Private
 
-# Allow WSL2 to access Windows Kubernetes Cluster
-iex "netsh interface portproxy add v4tov4 listenport=6443 connectaddress=127.0.0.1 connectport=6443 listenaddress=$wslAddress protocol=tcp"
+  # Allow WSL2 to access Windows Docker Daemon to share same images.
+  $wslAddress = (Get-NetIPAddress -InterfaceAlias "$adapter" -AddressFamily IPv4).IPAddress
+  iex "netsh interface portproxy add v4tov4 listenport=2375 connectaddress=127.0.0.1 connectport=2375 listenaddress=$wslAddress protocol=tcp"
+
+  # Allow WSL2 to access Windows Kubernetes Cluster
+  iex "netsh interface portproxy add v4tov4 listenport=6443 connectaddress=127.0.0.1 connectport=6443 listenaddress=$wslAddress protocol=tcp"
+}
 
 # check result
 netsh interface portproxy show v4tov4
+
+pause
