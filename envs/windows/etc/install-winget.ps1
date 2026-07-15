73f881b4aa497858422057d56b4e317d587b3d10
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2026-07-16 00:51:20 +0900

    chore: winget apps

--- envs/windows/etc/install-winget.ps1
+++ envs/windows/etc/install-winget.ps1
@@ -9,4 +9,7 @@ winget install -e --id Google.Chrome --accept-package-agreements
 winget install -e --id Microsoft.VisualStudioCode --override '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders"'
 winget install -e --id Microsoft.PowerShell --accept-package-agreements
 winget install -e --id Anysphere.Cursor --accept-package-agreements
-winget install -e --id Codex -s msstore --accept-package-agreements
+# chatgpt (codex)
+winget install -e --id 9PLM9XGG6VKS -s msstore --accept-package-agreements
+winget install -e --id Anthropic.Claude --accept-package-agreements
+winget install -e --id GitHub.CopilotApp --accept-package-agreements
